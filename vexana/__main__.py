2757cd2767abc292c3091f07c8ab07a595a4b37f
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2021-10-03 06:49:13 -0700

    Update __main__.py

--- vexana/__main__.py
+++ vexana/__main__.py
@@ -791,7 +791,7 @@ def main():
     settings_handler = CommandHandler("settings", get_settings)
     settings_callback_handler = CallbackQueryHandler(settings_button, pattern=r"stngs_")
 
-    about_callback_handler = CallbackQueryHandler(vexana_about_callback, pattern=r"vexana_")
+    about_callback_handler = CallbackQueryHandler(Shoko_about_callback, pattern=r"aboutmanu_")
     source_callback_handler = CallbackQueryHandler(Source_about_callback, pattern=r"source_")
 
     donate_handler = CommandHandler("donate", donate)
