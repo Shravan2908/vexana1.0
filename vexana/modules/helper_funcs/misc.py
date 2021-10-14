6a23f55696d88f37d9078ddc7140774bbfdff0fc
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2021-10-14 05:27:20 -0700

    Update misc.py
    bug fix

--- vexana/modules/helper_funcs/misc.py
+++ vexana/modules/helper_funcs/misc.py
@@ -66,9 +66,9 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 8:
         pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("❤Back❤", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("❤Close❤", callback_data="vexana_back"),
-             EqInlineKeyboardButton("❤Upcoming❤", callback_data="{}_next({})".format(prefix, modulo_page)))]
+            (EqInlineKeyboardButton("❤👈❤", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton("❤Back❤", callback_data="vexana_back"),
+             EqInlineKeyboardButton("❤👉❤", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
         pairs += [[EqInlineKeyboardButton("❤Back❤", callback_data="vexana_back")]]
