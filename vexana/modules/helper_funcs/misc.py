eb65b2510db8a75be2c48ef034844a2eebe48151
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2021-10-16 08:40:24 -0700

    Update misc.py
    patch1.0 Fix help back

--- vexana/modules/helper_funcs/misc.py
+++ vexana/modules/helper_funcs/misc.py
@@ -66,10 +66,10 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 8:
         pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("🔜Back🔜", callback_data="help_back""),)]
+            (EqInlineKeyboardButton("🔜Back🔜", callback_data="help_back"),)]
 
     else:
-        pairs += [[EqInlineKeyboardButton("🔜Back🔜", callback_data="help_back"")]]
+        pairs += [[EqInlineKeyboardButton("🔜Back🔜", callback_data="help_back")]]
 
     return pairs
 
