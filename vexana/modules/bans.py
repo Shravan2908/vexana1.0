6981a24fc7c8705945a0ac3ad22c0f494560628a
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2021-10-20 14:31:25 -0700

    button ptch1.0

--- vexana/modules/bans.py
+++ vexana/modules/bans.py
@@ -111,8 +111,7 @@ def ban(update: Update, context: CallbackContext) -> str:
         # bot.send_sticker(chat.id, BAN_STICKER)  # banhammer marie sticker
         reply = (
             f"<code>❕</code><b>Ban Event</b>\n"
-            f"©AXEL  Just Kicked A piece of  Shit! {mention_html(member.user.id, html.escape(member.user.first_name))}.",
-            parse_mode=ParseMode.HTML,
+            f"<code> </code><b>•  User:</b> {mention_html(member.user.id, html.escape(member.user.first_name))}"
         )
         if reason:
             reply += f"\n<code> </code><b>•  Reason:</b> \n{html.escape(reason)}"
