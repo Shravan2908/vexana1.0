215a86aca9a77b2183fd8cb7739fad45c3c08218
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2021-11-18 09:19:51 -0800

    Update in Levels

--- vexana/modules/userinfo.py
+++ vexana/modules/userinfo.py
@@ -288,22 +288,22 @@ def info(update: Update, context: CallbackContext):
     disaster_level_present = False
 
     if user.id == OWNER_ID:
-        text += "\n\nThe Disaster level of this person is 'OWNER'."
+        text += "\n\nThe access level of this person is 'OWNER'."
         disaster_level_present = True
     elif user.id in DEV_USERS:
         text += "\n\nThis user is member of 'VexanaFanClub'."
         disaster_level_present = True
     elif user.id in DRAGONS:
-        text += "\n\nThe Disaster level of this person is 'Dragon'."
+        text += "\n\nThe access level of this person is 'Vexana-Lover'."
         disaster_level_present = True
     elif user.id in DEMONS:
-        text += "\n\nThe Disaster level of this person is 'Demon'."
+        text += "\n\nThe access level of this person is 'Vexana-Supporter'."
         disaster_level_present = True
     elif user.id in TIGERS:
-        text += "\n\nThe Disaster level of this person is 'Tiger'."
+        text += "\n\nThe access level of this person is 'Vexana-Approval'."
         disaster_level_present = True
     elif user.id in WOLVES:
-        text += "\n\nThe Disaster level of this person is 'Wolf'."
+        text += "\n\nThe Disaster level of this person is 'Vexana-MOD."
         disaster_level_present = True
 
     if disaster_level_present:
