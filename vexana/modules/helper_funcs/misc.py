dd06d5bc5309acf9ef498b3146372035c98bf2a3
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2021-11-18 06:37:37 -0800

    Mistake Type Error

--- vexana/modules/helper_funcs/misc.py
+++ vexana/modules/helper_funcs/misc.py
@@ -74,7 +74,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs.append((modules[-1],))
 
     else:
-        pairs += [[EqInlineKeyboardButton("Back ", callback_data="help_back")]]
+        pairs += [[EqInlineKeyboardButton("Back ", callback_data="aboutmanu_back")]]
 
     return pairs
 
