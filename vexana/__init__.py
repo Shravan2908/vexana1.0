c2f40301b01e94055e9f78d2e0006fbe9fbd1cf7
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2021-11-18 07:16:08 -0800

    Update __init__.py

--- vexana/__init__.py
+++ vexana/__init__.py
@@ -41,7 +41,7 @@ if ENV:
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
 
     try:
-        SUDO_USERS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
+        DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
         DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "").split())
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
