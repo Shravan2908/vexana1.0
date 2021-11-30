5a05dfafbb638e7931a837bc3744f01b52ad0076
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2021-11-29 18:35:44 -0800

    Same Af

--- vexana/modules/helper_funcs/misc.py
+++ vexana/modules/helper_funcs/misc.py
@@ -64,7 +64,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
             ]
         )
 
-    pairs = [modules[i * 4 : (i + 1) * 3] for i in range((len(modules) + 4 - 1) // 4)]
+    pairs = [modules[i * 6 : (i + 1) * 3] for i in range((len(modules) + 6 - 1) // 6)]
 
     round_num = len(modules) / 3
     calc = len(modules) - round(round_num)
