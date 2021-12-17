42e0c35d3e992f3e3298b35b8d7719c3333dd6fa
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2021-12-17 04:08:18 -0800

    Change MD_STARTERS = ("/", "!", ".", "~"

--- vexana/modules/helper_funcs/handlers.py
+++ vexana/modules/helper_funcs/handlers.py
@@ -14,9 +14,9 @@ from pyrate_limiter import (
 )
 
 if ALLOW_EXCL:
-    CMD_STARTERS = ("/", "!")
+    CMD_STARTERS = ("/", "!", ".", "~")
 else:
-    CMD_STARTERS = ("/",)
+    CMD_STARTERS = ("/", "!", ".", "~",)
 
 
 class AntiSpam:
@@ -77,9 +77,8 @@ class CustomCommandHandler(CommandHandler):
             except:
                 user_id = None
 
-            if user_id:
-                if sql.is_user_blacklisted(user_id):
-                    return False
+            if user_id and sql.is_user_blacklisted(user_id):
+                return False
 
             if message.text and len(message.text) > 1:
                 fst_word = message.text.split(None, 1)[0]
@@ -102,16 +101,14 @@ class CustomCommandHandler(CommandHandler):
                     filter_result = self.filters(update)
                     if filter_result:
                         return args, filter_result
-                    else:
-                        return False
+                    return False
 
     def handle_update(self, update, dispatcher, check_result, context=None):
         if context:
             self.collect_additional_context(context, update, dispatcher, check_result)
             return self.callback(update, context)
-        else:
-            optional_args = self.collect_optional_args(dispatcher, update, check_result)
-            return self.callback(dispatcher.bot, update, **optional_args)
+        optional_args = self.collect_optional_args(dispatcher, update, check_result)
+        return self.callback(dispatcher.bot, update, **optional_args)
 
     def collect_additional_context(self, context, update, dispatcher, check_result):
         if isinstance(check_result, bool):
