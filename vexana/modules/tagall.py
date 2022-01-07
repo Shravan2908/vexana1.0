e3e6b5268bd2b29840e74f24194d055a834681ae
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2022-01-07 07:24:36 -0800

    Update tagall.py

--- vexana/modules/tagall.py
+++ vexana/modules/tagall.py
@@ -1,36 +1,86 @@
-# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
-#
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
-#
-# All rights reserved.
+import os
+import logging
+import asyncio
+
+from telethon import Button, TelegramClient, events
+from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.errors import UserNotParticipantError
+
+from vexana import telethn as Client
+
+spam_chats = []
 
 
-from pyrogram import filters
+@Client.on(events.NewMessage(pattern="^/all ?(.*)"))
+@Client.on(events.NewMessage(pattern="^@all ?(.*)"))
+async def mentionall(event):
+    chat_id = event.chat_id
+    if event.is_private:
+        return await event.respond("This command can be use in groups and channels!")
 
-from vexana.pyrogramee.pluginshelper import admins_only, get_text
-from vexana import pbot
+    is_admin = False
+    try:
+        partici_ = await Client(GetParticipantRequest(event.chat_id, event.sender_id))
+    except UserNotParticipantError:
+        is_admin = False
+    else:
+        if isinstance(
+            partici_.participant, (ChannelParticipantAdmin, ChannelParticipantCreator)
+        ):
+            is_admin = True
+    if not is_admin:
+        return await event.respond("Only admins can mention all!")
+
+    if event.pattern_match.group(1) and event.is_reply:
+        return await event.respond("Give me one argument!")
+    elif event.pattern_match.group(1):
+        mode = "text_on_cmd"
+        msg = event.pattern_match.group(1)
+    elif event.is_reply:
+        mode = "text_on_reply"
+        msg = await event.get_reply_message()
+        if msg is None:
+            return await event.respond(
+                "I can't mention members for older messages! (messages which are sent before I'm added to group)"
+            )
+    else:
+        return await event.respond(
+            "Reply to a message or give me some text to mention others!"
+        )
+
+    Spam = spam_chats.append(chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in Client.iter_participants(chat_id):
+        if chat_id not in spam_chats:
+            break
+        usrnum += 1
+        usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+        if usrnum == 5:
+            if mode == "text_on_cmd":
+                txt = f"{usrtxt}\n\n{msg}"
+                await Client.send_message(chat_id, txt)
+            elif mode == "text_on_reply":
+                await msg.reply(usrtxt)
+            await asyncio.sleep(2)
+            usrnum = 0
+            usrtxt = ""
+    try:
+        spam_chats.remove(chat_id)
+    except:
+        pass
 
 
-@pbot.on_message(filters.command("tagall") & ~filters.edited & ~filters.bot)
-@admins_only
-async def tagall(client, message):
-    await message.reply("`Processing.....`")
-    sh = get_text(message)
-    if not sh:
-        sh = "Hi!"
-    mentions = ""
-    async for member in client.iter_chat_members(message.chat.id):
-        mentions += member.user.mention + " "
-    n = 4096
-    kk = [mentions[i : i + n] for i in range(0, len(mentions), n)]
-    for i in kk:
-        j = f"<b>{sh}</b> \n{i}"
-        await client.send_message(message.chat.id, j, parse_mode="html")
+@Client.on(events.NewMessage(pattern="^/cancel$"))
+async def cancel_spam(event):
+    if event.chat_id not in spam_chats:
+        return await event.respond("There is no proccess on going...")
+    try:
+        spam_chats.remove(event.chat_id)
+    except:
+        pass
+    return await event.respond("Stopped.")
 
 
-__mod_name__ = "Tagall"
-__help__ = """
-- /tagall : Tag everyone in a chat
-"""
+__mod_name__ = "Mentions"
