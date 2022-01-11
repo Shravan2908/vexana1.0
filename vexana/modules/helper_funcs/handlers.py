37c621932611aee8a41b5c0c671e3923fc007b00
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2022-01-11 07:47:42 -0800

    Patched Db insertation
    def __init__(self, filters, callback, friendly="", allow_edit=False, **kwargs):
            super().__init__(filters, callback, **kwargs)
            if allow_edit is False:
                self.filters &= ~(
                    Filters.update.edited_message | Filters.update.edited_channel_post

--- vexana/modules/helper_funcs/handlers.py
+++ vexana/modules/helper_funcs/handlers.py
@@ -1,18 +1,31 @@
-import re
-import telegram.ext as tg
-from telegram import Update
 import vexana.modules.sql.blacklistusers_sql as sql
-from vexana import DEV_USERS, SUDO_USERS
-from pyrate_limiter import (BucketFullException, Duration, RequestRate, Limiter,
-                            MemoryListBucket)
+from vexana import ALLOW_EXCL
+from vexana import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
+
+from telegram import Update
+from telegram.ext import CommandHandler, MessageHandler, RegexHandler, Filters
+from time import sleep
+from pyrate_limiter import (
+    BucketFullException,
+    Duration,
+    RequestRate,
+    Limiter,
+    MemoryListBucket,
+)
 
 CMD_STARTERS = ("/", "!", ".", "?")
 
-class AntiSpam:
 
+class AntiSpam:
     def __init__(self):
-        self.whitelist = (list(SUDO_USERS) or []) + (list(DEV_USERS) or [])
-        #Values are HIGHLY experimental, its recommended you pay attention to our commits as we will be adjusting the values over time with what suits best.
+        self.whitelist = (
+            (DEV_USERS or [])
+            + (DRAGONS or [])
+            + (WOLVES or [])
+            + (DEMONS or [])
+            + (TIGERS or [])
+        )
+        # Values are HIGHLY experimental, its recommended you pay attention to our commits as we will be adjusting the values over time with what suits best.
         Duration.CUSTOM = 15  # Custom duration, 15 seconds
         self.sec_limit = RequestRate(6, Duration.CUSTOM)  # 6 / Per 15 Seconds
         self.min_limit = RequestRate(20, Duration.MINUTE)  # 20 / Per minute
@@ -23,7 +36,8 @@ class AntiSpam:
             self.min_limit,
             self.hour_limit,
             self.daily_limit,
-            bucket_class=MemoryListBucket)
+            bucket_class=MemoryListBucket,
+        )
 
     def check_user(self, user):
         """
@@ -39,81 +53,83 @@ class AntiSpam:
 
 
 SpamChecker = AntiSpam()
+MessageHandlerChecker = AntiSpam()
 
-class CustomCommandHandler(tg.CommandHandler):
-    def __init__(self, command, callback, **kwargs):
-        if "admin_ok" in kwargs:
-            del kwargs["admin_ok"]
+
+class CustomCommandHandler(CommandHandler):
+    def __init__(self, command, callback, admin_ok=False, allow_edit=False, **kwargs):
         super().__init__(command, callback, **kwargs)
 
+        if allow_edit is False:
+            self.filters &= ~(
+                Filters.update.edited_message | Filters.update.edited_channel_post
+            )
+
     def check_update(self, update):
-        if (isinstance(update, Update)
-                and (update.message or update.edited_message and self.allow_edited)):
-            message = update.message or update.edited_message
-            if update.effective_user:
-                if sql.is_user_blacklisted(update.effective_user.id):
-                    return False
+        if not isinstance(update, Update) or not update.effective_message:
+            return
+        message = update.effective_message
 
-            if message.text and len(message.text) > 1:
-                fst_word = message.text_html.split(None, 1)[0]
-                if len(fst_word) > 1 and any(fst_word.startswith(start) for start in CMD_STARTERS):
-                    command = fst_word[1:].split('@')
-                    command.append(message.bot.username)  # in case the command was sent without a username
-                    if self.filters is None:
-                        res = True
-                    elif isinstance(self.filters, list):
-                        res = any(func(message) for func in self.filters)
-                    else:
-                        res = self.filters(message)
-                    if command[0].lower() in self.command and command[1].lower() == message.bot.username.lower():
-                        if SpamChecker.check_user(update.effective_user.id):
-                            return None
-                    return res and (command[0].lower() in self.command
-                                    and command[1].lower() == message.bot.username.lower())
+        try:
+            user_id = update.effective_user.id
+        except:
+            user_id = None
 
+        if user_id and sql.is_user_blacklisted(user_id):
             return False
 
+        if message.text and len(message.text) > 1:
+            fst_word = message.text.split(None, 1)[0]
+            if len(fst_word) > 1 and any(
+                fst_word.startswith(start) for start in CMD_STARTERS
+            ):
 
-class CustomRegexHandler(tg.RegexHandler):
+                args = message.text.split()[1:]
+                command = fst_word[1:].split("@")
+                command.append(message.bot.username)
+                if user_id == 1087968824:
+                    user_id = update.effective_chat.id
+                if not (
+                    command[0].lower() in self.command
+                    and command[1].lower() == message.bot.username.lower()
+                ):
+                    return None
+                if SpamChecker.check_user(user_id):
+                    return None
+                filter_result = self.filters(update)
+                if filter_result:
+                    return args, filter_result
+                return False
+
+    def handle_update(self, update, dispatcher, check_result, context=None):
+        if context:
+            self.collect_additional_context(context, update, dispatcher, check_result)
+            return self.callback(update, context)
+        optional_args = self.collect_optional_args(dispatcher, update, check_result)
+        return self.callback(dispatcher.bot, update, **optional_args)
+
+    def collect_additional_context(self, context, update, dispatcher, check_result):
+        if isinstance(check_result, bool):
+            context.args = update.effective_message.text.split()[1:]
+        else:
+            context.args = check_result[0]
+            if isinstance(check_result[1], dict):
+                context.update(check_result[1])
+
+
+class CustomRegexHandler(RegexHandler):
     def __init__(self, pattern, callback, friendly="", **kwargs):
         super().__init__(pattern, callback, **kwargs)
 
-    def check_update(self, update):
-        if isinstance(update, Update) and update.effective_message:
-            if update.effective_user:
-                if sql.is_user_blacklisted(update.effective_user.id):
-                    return False
-        else:
-            return False
-        if any([self.message_updates and update.message,
-                    self.edited_updates and (update.edited_message or update.edited_channel_post),
-                    self.channel_post_updates and update.channel_post]) and \
-                    update.effective_message.text:
-                match = re.match(self.pattern, update.effective_message.text)
-                return bool(match)
-        return False
 
-
-class CustomMessageHandler(tg.MessageHandler):
-    def __init__(self, filters, callback, **kwargs):
+class CustomMessageHandler(MessageHandler):
+    def __init__(self, filters, callback, friendly="", allow_edit=False, **kwargs):
         super().__init__(filters, callback, **kwargs)
+        if allow_edit is False:
+            self.filters &= ~(
+                Filters.update.edited_message | Filters.update.edited_channel_post
+            )
 
-    def check_update(self, update):
-        if isinstance(update, Update) and self._is_allowed_update(update):
-            if update.effective_user:
-                if sql.is_user_blacklisted(update.effective_user.id):
-                    return False
-            if self.filters is None:
-                res = True
-
-            else:
-                message = update.effective_message
-                if isinstance(self.filters, list):
-                    res = any(func(message) for func in self.filters)
-                else:
-                    res = self.filters(message)
-
-        else:
-            res = False
-
-        return res
+        def check_update(self, update):
+            if isinstance(update, Update) and update.effective_message:
+                return self.filters(update)
