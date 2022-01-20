62aacd964e47d91c1343492334a22afd3ad613b3
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2022-01-20 01:36:11 -0800

    admin cmd Enable
    # For admins and sudo users
                userID = message.from_user.id
                permissions = await member_permissions(chatID, userID)
                if userID not in DEMONS and permission not in permissions:
                    return await unauthorised(message, permission, subFunc2)
                return await authorised(
                    func, subFunc2, client, message, *args, **kwargs

--- vexana/utils/permissions.py
+++ vexana/utils/permissions.py
@@ -4,7 +4,7 @@ from traceback import format_exc as err
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 from pyrogram.types import Message
 
-from vexana import SUDOERS, pbot
+from vexana import DEMONS, pbot
 from vexana.modules.admin import member_permissions
 
 
@@ -60,7 +60,7 @@ def adminsOnly(permission):
             # For admins and sudo users
             userID = message.from_user.id
             permissions = await member_permissions(chatID, userID)
-            if userID not in SUDOERS and permission not in permissions:
+            if userID not in DEMONS and permission not in permissions:
                 return await unauthorised(message, permission, subFunc2)
             return await authorised(
                 func, subFunc2, client, message, *args, **kwargs
