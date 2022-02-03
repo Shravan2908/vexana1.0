8f40f926437adcacf76aead8a5e3640917882538
aashish <aksraashish@gmail.com>
2022-02-02 19:12:18 -0800

    Bump Something Something

--- vexana/modules/log_channel.py
+++ vexana/modules/log_channel.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from functools import wraps
+
 from telegram.ext import CallbackContext
+
 from vexana.modules.helper_funcs.misc import is_module_loaded
 
 FILENAME = __name__.rsplit(".", 1)[-1]
@@ -8,13 +10,12 @@ FILENAME = __name__.rsplit(".", 1)[-1]
 if is_module_loaded(FILENAME):
     from telegram import ParseMode, Update
     from telegram.error import BadRequest, Unauthorized
-    from telegram.ext import CommandHandler, JobQueue, run_async
+    from telegram.ext import CommandHandler, JobQueue
     from telegram.utils.helpers import escape_markdown
 
-    from vexana import EVENT_LOGS, LOGGER, dispatcher
-    from vexana.modules.helper_funcs.chat_status import user_admin
-    from vexana.modules.sql import log_channel_sql as sql
-    from vexana.modules.language import gs
+    from NekoRobot import EVENT_LOGS, LOGGER, dispatcher
+    from NekoRobot.modules.helper_funcs.chat_status import user_admin
+    from NekoRobot.modules.sql import log_channel_sql as sql
 
     def loggable(func):
         @wraps(func)
@@ -57,7 +58,7 @@ if is_module_loaded(FILENAME):
             if result:
                 datetime_fmt = "%H:%M - %d-%m-%Y"
                 result += "\n<b>Event Stamp</b>: <code>{}</code>".format(
-                    datetime.utcnow().strftime(datetime_fmt)
+                    datetime.utcnow().strftime(datetime_fmt),
                 )
 
                 if message.chat.type == chat.SUPERGROUP and message.chat.username:
@@ -71,7 +72,10 @@ if is_module_loaded(FILENAME):
         return glog_action
 
     def send_log(
-        context: CallbackContext, log_chat_id: str, orig_chat_id: str, result: str
+        context: CallbackContext,
+        log_chat_id: str,
+        orig_chat_id: str,
+        result: str,
     ):
         bot = context.bot
         try:
@@ -84,7 +88,8 @@ if is_module_loaded(FILENAME):
         except BadRequest as excp:
             if excp.message == "Chat not found":
                 bot.send_message(
-                    orig_chat_id, "This log channel has been deleted - unsetting."
+                    orig_chat_id,
+                    "This log channel has been deleted - unsetting.",
                 )
                 sql.stop_chat_logging(orig_chat_id)
             else:
@@ -123,7 +128,7 @@ if is_module_loaded(FILENAME):
         chat = update.effective_chat
         if chat.type == chat.CHANNEL:
             message.reply_text(
-                "Now, forward the /setlog to the group you want to tie this channel to!"
+                "Now, forward the /setlog to the group you want to tie this channel to!",
             )
 
         elif message.forward_from_chat:
@@ -131,11 +136,9 @@ if is_module_loaded(FILENAME):
             try:
                 message.delete()
             except BadRequest as excp:
-                if excp.message == "Message to delete not found":
-                    pass
-                else:
+                if excp.message != "Message to delete not found":
                     LOGGER.exception(
-                        "Error deleting message in log channel. Should work anyway though."
+                        "Error deleting message in log channel. Should work anyway though.",
                     )
 
             try:
@@ -156,7 +159,7 @@ if is_module_loaded(FILENAME):
                 "The steps to set a log channel are:\n"
                 " - add bot to the desired channel\n"
                 " - send /setlog to the channel\n"
-                " - forward the /setlog to the group\n"
+                " - forward the /setlog to the group\n",
             )
 
     @user_admin
@@ -168,7 +171,8 @@ if is_module_loaded(FILENAME):
         log_channel = sql.stop_chat_logging(chat.id)
         if log_channel:
             bot.send_message(
-                log_channel, f"Channel has been unlinked from {chat.title}"
+                log_channel,
+                f"Channel has been unlinked from {chat.title}",
             )
             message.reply_text("Log channel has been un-set.")
 
@@ -176,7 +180,7 @@ if is_module_loaded(FILENAME):
             message.reply_text("No log channel has been set yet!")
 
     def __stats__():
-        return f"× {sql.num_logchannels()} log channels set."
+        return f"• {sql.num_logchannels()} log channels set."
 
     def __migrate__(old_chat_id, new_chat_id):
         sql.migrate_chat(old_chat_id, new_chat_id)
@@ -188,11 +192,19 @@ if is_module_loaded(FILENAME):
             return f"This group has all it's logs sent to: {escape_markdown(log_channel_info.title)} (`{log_channel}`)"
         return "No log channel is set for this group!"
 
+    __help__ = """
+*Admins only:*
+• `/logchannel`*:* get log channel info
+• `/setlog`*:* set the log channel.
+• `/unsetlog`*:* unset the log channel.
 
-    def helps(chat):
-        return gs(chat, "logchannel_help")
+Setting the log channel is done by:
+• adding the bot to the desired channel (as an admin!)
+• sending `/setlog` in the channel
+• forwarding the `/setlog` to the group
+"""
 
-    __mod_name__ = "Log Channel​"
+    __mod_name__ = "Logger"
 
     LOG_HANDLER = CommandHandler("logchannel", logging, run_async=True)
     SET_LOG_HANDLER = CommandHandler("setlog", setlog, run_async=True)
