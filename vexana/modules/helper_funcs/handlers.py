2c958c36690ada2adc489c8dee7f473ef09dfa16
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2022-02-12 04:58:57 -0800

    hm typo

--- vexana/modules/helper_funcs/handlers.py
+++ vexana/modules/helper_funcs/handlers.py
@@ -1,5 +1,4 @@
 import vexana.modules.sql.blacklistusers_sql as sql
-
 from vexana import ALLOW_EXCL
 from vexana import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
 
@@ -13,17 +12,20 @@ from pyrate_limiter import (
     MemoryListBucket,
 )
 
-CMD_STARTERS = ("/", "!") if ALLOW_EXCL else ("/",)
+if ALLOW_EXCL:
+    CMD_STARTERS = ("/", "!", ".", "~")
+else:
+    CMD_STARTERS = ("/", "!", ".", "~",)
 
 
 class AntiSpam:
     def __init__(self):
         self.whitelist = (
-                (DEV_USERS or [])
-                + (DRAGONS or [])
-                + (WOLVES or [])
-                + (DEMONS or [])
-                + (TIGERS or [])
+            (DEV_USERS or [])
+            + (DRAGONS or [])
+            + (WOLVES or [])
+            + (DEMONS or [])
+            + (TIGERS or [])
         )
         # Values are HIGHLY experimental, its recommended you pay attention to our commits as we will be adjusting the values over time with what suits best.
         Duration.CUSTOM = 15  # Custom duration, 15 seconds
@@ -62,52 +64,49 @@ class CustomCommandHandler(CommandHandler):
 
         if allow_edit is False:
             self.filters &= ~(
-                    Filters.update.edited_message | Filters.update.edited_channel_post
+                Filters.update.edited_message | Filters.update.edited_channel_post
             )
 
     def check_update(self, update):
-        if not isinstance(update, Update) or not update.effective_message:
-            return
-        message = update.effective_message
+        if isinstance(update, Update) and update.effective_message:
+            message = update.effective_message
 
-        try:
-            user_id = update.effective_user.id
-        except:
-            user_id = None
+            try:
+                user_id = update.effective_user.id
+            except:
+                user_id = None
 
-        if user_id and sql.is_user_blacklisted(user_id):
-            return False
+            if user_id and sql.is_user_blacklisted(user_id):
+                return False
 
-        if message.text and len(message.text) > 1:
-            fst_word = message.text.split(None, 1)[0]
-            if len(fst_word) > 1 and any(
+            if message.text and len(message.text) > 1:
+                fst_word = message.text.split(None, 1)[0]
+                if len(fst_word) > 1 and any(
                     fst_word.startswith(start) for start in CMD_STARTERS
-            ):
+                ):
 
-                args = message.text.split()[1:]
-                command = fst_word[1:].split("@")
-                command.append(message.bot.username)
-                if user_id == 1087968824:
-                    user_id = update.effective_chat.id
-                if not (
+                    args = message.text.split()[1:]
+                    command = fst_word[1:].split("@")
+                    command.append(message.bot.username)
+                    if user_id == 1087968824:
+                        user_id = update.effective_chat.id
+                    if not (
                         command[0].lower() in self.command
                         and command[1].lower() == message.bot.username.lower()
-                ):
-                    return None
-                if SpamChecker.check_user(user_id):
-                    return None
-                filter_result = self.filters(update)
-                if filter_result:
-                    return args, filter_result
-                return False
+                    ):
+                        return None
+                    if SpamChecker.check_user(user_id):
+                        return None
+                    filter_result = self.filters(update)
+                    if filter_result:
+                        return args, filter_result
+                    return False
 
     def handle_update(self, update, dispatcher, check_result, context=None):
         if context:
-            self.collect_additional_context(context, update, dispatcher,
-                                            check_result)
+            self.collect_additional_context(context, update, dispatcher, check_result)
             return self.callback(update, context)
-        optional_args = self.collect_optional_args(dispatcher, update,
-                                                   check_result)
+        optional_args = self.collect_optional_args(dispatcher, update, check_result)
         return self.callback(dispatcher.bot, update, **optional_args)
 
     def collect_additional_context(self, context, update, dispatcher, check_result):
@@ -129,7 +128,7 @@ class CustomMessageHandler(MessageHandler):
         super().__init__(filters, callback, **kwargs)
         if allow_edit is False:
             self.filters &= ~(
-                    Filters.update.edited_message | Filters.update.edited_channel_post
+                Filters.update.edited_message | Filters.update.edited_channel_post
             )
 
         def check_update(self, update):
