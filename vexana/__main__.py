84858ef1d5b989faa33c21f9ad49aabaf0a412c3
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2022-02-18 10:44:08 -0800

    hm

--- vexana/__main__.py
+++ vexana/__main__.py
@@ -37,7 +37,7 @@ from vexana.events import register
 from vexana.modules import ALL_MODULES
 from vexana.modules.helper_funcs.chat_status import is_user_admin
 from vexana.modules.helper_funcs.alternate import typing_action
-from vexana.modules.helper_funcs.alternate import 
+from vexana.modules.helper_funcs.readable_time  import get_readable_time
 from vexana.modules.helper_funcs.misc import paginate_modules
 from vexana.modules.disable import DisableAbleCommandHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
